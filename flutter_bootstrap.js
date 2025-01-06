4fdd634d2b8ede4777517cb4a45cd97f549f1e61
Brent-styles <141153097+Brent-styles@users.noreply.github.com>
2025-01-06 21:57:22 +0200

    update login and members

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4276268770"
+    serviceWorkerVersion: "2058036025"
   }
 });
